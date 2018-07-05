4135f97bdd33e3066b4dcd210bf97ea324d23617
scrappy1987 <jagordon87@msn.com>
2018-07-05 16:12:53 +0100

    failing tests

---
+++ src/test/java/com/qa/calculator/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.qa.calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void test() {
+		Calculator calc = new Calculator();
+		int actualValue = calc.add(2, 2);
+		Assert.assertEquals(4, actualValue);
+	}
+
+}

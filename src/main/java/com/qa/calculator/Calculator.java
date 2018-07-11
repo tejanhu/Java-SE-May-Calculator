c83d5555402c77867223c5c7e38e40c68303f45c
Hussein Tejan <tejanahmedhu@gmail.com>
2018-07-11 12:10:56 +0100

    REFACTORED - ALL GREEN

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -2,8 +2,10 @@ package com.qa.calculator;
 
 public class Calculator {
 
+	Functionality adder = new Functionality();
+	
 	public int add(int num1, int num2) {
-		return num1 + num2;
+		return adder.add(num1, num2);
 	}
 
 }

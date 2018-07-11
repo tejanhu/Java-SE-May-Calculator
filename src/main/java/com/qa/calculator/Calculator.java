752526e34f6cfd0692f1cfae6c03db2022a730d8
Hussein Tejan <tejanahmedhu@gmail.com>
2018-07-11 12:15:37 +0100

    MULTIPLY WORKING - REFACTORED

--- src/main/java/com/qa/calculator/Calculator.java
+++ src/main/java/com/qa/calculator/Calculator.java
@@ -1,13 +1,15 @@
 package com.qa.calculator;
 
 public class Calculator {
+	
+	Functionality multiplier = new Functionality();
 
 	public int add(int num1, int num2) {
 		return num1 + num2;
 	}
 
 	public int multiply(int num1, int num2) {
-		return num1 * num2;
+		return multiplier.multiply(num1, num2);
 	}
 
 }
